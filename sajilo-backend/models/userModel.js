433e1cad78db50f7eae385f290343a5cfaf821d1
AdarshBajracharya <abajracharya55@gmail.com>
2025-07-30 08:35:28 +0545

    models

---
+++ sajilo-backend/models/userModel.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: false
+    },
+    // gender: {
+    //     type: String,
+    //     required: false
+    // },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+        required: false
+    },
+    // phone: {
+    //     type: String,
+    //     required: true
+    // },
+    otp: {
+        type: String,
+        required: false 
+    },
+    failedLoginAttempts: {
+    type: Number,
+    default: 0
+  },
+  isLocked: {
+    type: Boolean,
+    default: false
+  },
+  lockUntil: {
+    type: Date,
+    default: null
+  }
+})
+const user = mongoose.model('users', userSchema)
+module.exports = user;
